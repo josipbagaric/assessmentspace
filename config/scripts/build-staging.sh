024743a460802e44b9d0d891e8d68d7fa9b198c1
Bagaric <bagaricjos@gmail.com>
2017-05-07 20:35:23 +1200

    Modified build script

--- config/scripts/build-staging.sh
+++ config/scripts/build-staging.sh
@@ -2,7 +2,7 @@
 rm -rf ~/assessmentspace
 git clone git@github.com:Bagaric/assessmentspace.git ~/assessmentspace
 cd ~/assessmentspace
-python -m virtualenv --python=/usr/bin/python3 venv
+virtualenv --python=/usr/bin/python3 venv
 . venv/bin/activate
 pip install -r config/requirements.txt
 pip uninstall -y docker docker-py docker-compose
