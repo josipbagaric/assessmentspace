9f2dbe196362e1ba831e99520218904006fc114c
Bagaric <bagaricjos@gmail.com>
2017-06-11 16:05:13 +1200

    Added data loading to docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,5 @@ sleep 10
 python manage.py makemigrations
 python manage.py migrate_schemas
 python manage.py collectstatic --noinput
+python manage.py loaddata data.json
 gunicorn app.wsgi:application -w 1 -b 0.0.0.0:8000 --log-level=debug -e ENV=prod
